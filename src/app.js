c0ffd40f2f7d13b64d345d02d3b99bcf1b7b4241
Mariana <101410421+Placito@users.noreply.github.com>
2023-06-20 11:23:12 +0000

    secondsecond try

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+//How to place text in the browser window
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btnn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+//Create a function to generate random excuses
+const generateExcuse = () => {
+  //the variables with the various arguments that can be used
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //make these variables random values
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+
+  //put the various excuses together to form a sentence
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex] +
+    "."
+  );
 };
